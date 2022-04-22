00142575ef5a67ee3bca167313aff1e18ec97c5c
RomanKhmil <Khmilroman001@gmail.com>
2022-04-23 01:19:21 +0300

    ConnectCalcToAnalaize

--- Calculator/CalcClass/CalcClass.cs
+++ Calculator/CalcClass/CalcClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CalcClass
 {
-    internal class CalcClass
+    public class CalcClass
     {
         public static string lastError { get; private set; }
 
